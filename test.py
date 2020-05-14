a66998168eb7594a233b521b72d0a91323cc50e1
gglue <generalglue@gmail.com>
2020-05-14 07:57:52 -0400

    Revert "Rewrote README and deleted test file"
    This reverts commit ab3ee9af6607549b6e2a7e688b03d1e8e75b0fcc.

---
+++ test.py
@@ -0,0 +1,8 @@
+currentLevel = 19
+completeTiles = 156
+totalTiles = 172
+solvedLevels = 18
+
+test ="%11s%3d%22d/%d%20s%3d" % ("LEVEL", currentLevel, completeTiles,\
+                                                  totalTiles, "SOLVED", solvedLevels)
+print(test)
